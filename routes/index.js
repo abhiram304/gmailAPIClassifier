6b3e25d52f062fc5798d34ab7e58b8c7d81cb8b3
abhiram304 <abhiram.304@gmail.com>
2017-05-12 16:17:54 -0700

    fixed gmail auth error

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,8 @@ exports.index = function(req, res){
 	// If modifying these scopes, delete your previously saved credentials
 	// at ~/.credentials/gmail-nodejs-quickstart.json
 	var SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
-	var TOKEN_DIR = '/.credentials/';
-	var TOKEN_PATH = TOKEN_DIR + 'gmail-nodejs-quickstart.json';
+	var TOKEN_DIR = '';
+	var TOKEN_PATH = './abb.json';
 
 	// Load client secrets from a local file.
 	fs.readFile('client_secret.json', function processClientSecrets(err, content) {
@@ -88,13 +88,13 @@ exports.index = function(req, res){
 	 * @param {Object} token The token to store to disk.
 	 */
 	function storeToken(token) {
-	  try {
+	  /*try {
 	    fs.mkdirSync(TOKEN_DIR);
 	  } catch (err) {
 	    if (err.code != 'EEXIST') {
 	      throw err;
 	    }
-	  }
+	  }*/
 	  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
 	  console.log('Token stored to ' + TOKEN_PATH);
 	}
@@ -129,19 +129,25 @@ exports.index = function(req, res){
                   }
                   catch (Exception ex) { }
               }*/
-			  console.log("Labels: "+response.labelIds);
+			  
 			  //Snippet
 			  console.log("Snippet: "+response.snippet);
-			  console.log("Object 0: "+JSON.stringify(response.payload.headers[0]));
+			  //console.log("Object 0: "+JSON.stringify(response.payload.headers[0]));
 			  for(var i=0;i<(response.payload.headers).length;i++){
 				  console.log(" obj: "+JSON.stringify(response.payload.headers[i]));
 			  }
 			  //console.log("Received from "+JSON.stringify(response.payload.headers[12].value));
-			  console.log(new Buffer(response.payload.parts[0].body.data, 'base64').toString());
-			  
-			 console.log("Payload: "+JSON.stringify(response.payload.parts[0].body.data));
+			  //console.log(new Buffer(response.payload.parts, 'base64').toString());
+			  console.log("-----------------------------"+JSON.stringify(response.payload));
+			 //console.log("Payload: "+JSON.stringify(response.payload.parts[0].body.data));
 			 var resp = null;
-             for (var i=0; i<response.payload.headers.length; i++)
+			 var subject = null;
+			 var googleLabel = null;
+			 console.log("Labels: "+JSON.stringify(response));
+			 if (response.labelIds.indexOf("IMPORTANT")>0){
+				 googleLabel  = "important";
+			 }
+			 for (var i=0; i<response.payload.headers.length; i++)
              {   
                  if (response.payload.headers[i].name =="From")
                      {   var sender= response.payload.headers[i].value;
@@ -157,8 +163,26 @@ exports.index = function(req, res){
                         	 resp = "Unknown Sender";
                         	 console.log("Unknown Sender");}
                      }
+                 if(response.payload.headers[i].name =="Subject"){
+                	 
+                	 if(response.payload.headers[i].value != ""){
+                		 subject = response.payload.headers[i].value;
+                	 }
+                	 else{
+                		 subject = "This mail doesn't have a subject";
+                	 }
+                 }
              }
-			 res.send({"response": resp});
+			 //Classify here
+			 //http://ec2-54-173-121-109.compute-1.amazonaws.com:5000
+			 var request = require('request');
+			 request('http://ec2-54-173-121-109.compute-1.amazonaws.com:5000/?sentence=I%am%doing%good', function (error, response, body) {
+			   console.log('error:', error); // Print the error if one occurred 
+			   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+			   console.log('body:', body); // Print the HTML for the Google homepage. 
+			   res.send({"mail from ": resp, "subject": subject, "googleLabel" : googleLabel, "messageSnippet": response.snippet, "classifiedInto": body  });
+			 });
+			 
 			 //res.render('index', { title: 'Express' });  	
 			  }
 		  });
